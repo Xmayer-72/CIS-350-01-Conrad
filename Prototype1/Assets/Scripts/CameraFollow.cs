020f2eac346783396eb28375d2acda2598d7574a
Xmayer-72 <conrad.mayer72@gmail.com>
2022-01-24 17:53:11 -0600

    Initial work

---
+++ Prototype1/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Conrad Mayer
+ * 1/24/22
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 offset = new Vector3(0, 5, -7);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
