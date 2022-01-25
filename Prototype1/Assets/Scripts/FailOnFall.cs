bea54b1f91aedca2fc9c8b1bc85eda5c2a158acc
Xmayer-72 <conrad.mayer72@gmail.com>
2022-01-25 12:39:25 -0600

    Day 2 Work Initial

--- Prototype1/Assets/Scripts/FailOnFall.cs
+++ Prototype1/Assets/Scripts/FailOnFall.cs
@@ -6,19 +6,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 //Attatch to player
 public class FailOnFall : MonoBehaviour
 {
-    public Text textBox;
 
     // Update is called once per frame
     void Update()
     {
         if (transform.position.y < -1)
         {
-            textBox.text = "you Lose";
+            ScoreManager.gameOver = true;
         }
     }
 }
--- Prototype1/Assets/Scripts/TriggerCollision.cs
+++ Prototype1/Assets/Scripts/TriggerCollision.cs
@@ -6,18 +6,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class TriggerCollision : MonoBehaviour
 {
-    public Text textBox;
-
+    private bool triggered;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Finish"))
+        if (other.CompareTag("Player") && !triggered)
         {
-            //set text
-            textBox.text = "You Won!";
+            triggered = true;
+            ScoreManager.score++;
         }
     }
 }
