cf493b72c41a59dd0e6bc8ca88957f4148902763
Xmayer-72 <conrad.mayer72@gmail.com>
2022-01-25 13:00:45 -0600

    Minor Tweaks

--- Prototype1/Assets/Scripts/PLayerController.cs
+++ Prototype1/Assets/Scripts/PLayerController.cs
@@ -14,7 +14,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
