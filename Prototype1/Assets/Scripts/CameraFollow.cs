746c06a5460acc1d96269bbf34747d4e3416903e
Xmayer-72 <conrad.mayer72@gmail.com>
2022-01-25 12:46:54 -0600

    Added camera latency feature

--- Prototype1/Assets/Scripts/CameraFollow.cs
+++ Prototype1/Assets/Scripts/CameraFollow.cs
@@ -11,11 +11,16 @@ public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
 
+    public int speed;
+
     private Vector3 offset = new Vector3(0, 5, -7);
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + offset;
+        //if (transform.position != player.transform.position + offset)
+        Vector3 diff = transform.position - (player.transform.position + offset);
+
+        transform.Translate(-diff * Time.deltaTime * speed);
     }
 }
