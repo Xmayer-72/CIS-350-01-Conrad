c6b734a1da34f9f1c932fbd65303bd56ba598f18
Xmayer-72 <conrad.mayer72@gmail.com>
2022-02-01 10:15:29 -0600

    Morning Work Prototype2

---
+++ Prototype2/Assets/Scripts/DisplayScore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayScore : MonoBehaviour
+{
+    public Text textBox;
+
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+
+        textBox = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textBox.text = "Score: " + score;
+    }
+}
---
+++ Prototype2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] prefabs;
+    public bool gameOver;
+
+    private float leftBound = -14, rightBound = 14, zPosition = 20;
+
+    void Start()
+    {
+        gameOver = false;
+        StartCoroutine(SpawnRandomPrefabCoroutine());
+    }
+
+    IEnumerator SpawnRandomPrefabCoroutine()
+    {
+        yield return new WaitForSeconds(3f);
+
+        while (!gameOver)
+        {
+            SpawnRandomPrefab();
+
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+
+    void SpawnRandomPrefab()
+    {
+        int prefabIndex = Random.Range(0, prefabs.Length);
+
+        Vector4 spawnPos = new Vector3(Random.Range(leftBound, rightBound), 0, zPosition);
+
+        Instantiate(prefabs[prefabIndex], spawnPos, prefabs[prefabIndex].transform.rotation);
+    }
+}
