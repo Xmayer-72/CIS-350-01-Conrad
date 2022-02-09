455a25f4a138ecf68e903c98524facdd5e87d235
Xmayer-72 <conrad.mayer72@gmail.com>
2022-02-09 15:34:05 -0600

    Challenge 3 Work

---
+++ Challenge 3/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+    public int score = 0;
+
+    public float floatForce, bounce;
+    private float gravityModifier = 1.5f;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+    public AudioClip boingSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = gameObject.GetComponent<Rigidbody>();
+
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+
+        // Apply a small upward force at the start of the game
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // While space is pressed and player is low enough, float up
+        if (Input.GetButton("Jump") && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * floatForce);
+        }
+
+        if (transform.position.y > 14)
+        {
+            playerRb.velocity = new Vector3(0,0,0);
+            transform.position = new Vector3 (transform.position.x, 14, transform.position.z);
+        }
+
+        if (gameOver && Input.GetButtonDown("Submit"))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if player collides with bomb, explode and set gameOver to true
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            explosionParticle.Play();
+            playerAudio.PlayOneShot(explodeSound, 1.0f);
+            gameOver = true;
+            Debug.Log("Game Over!");
+            Destroy(other.gameObject);
+        }
+
+        // if player collides with money, fireworks
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1.0f);
+            score++;
+            Destroy(other.gameObject);
+
+        }
+
+        else if (other.gameObject.CompareTag("Background"))
+        {
+            playerRb.velocity = new Vector3(0, 0, 0);
+            playerRb.AddForce(Vector3.up * bounce);
+            playerAudio.PlayOneShot(boingSound, 1f);
+        }
+
+    }
+}
---
+++ Challenge 3/Assets/Challenge 3/Scripts/UIWriter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIWriter : MonoBehaviour
+{
+    private PlayerControllerX level;
+    private Text textBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        level = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControllerX>();
+        textBox = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (level.score>=30)
+        {
+            textBox.text = "You win! Press Enter to Restart!";
+            level.gameOver = true;
+        }
+        else if (level.gameOver)
+        {
+            textBox.text = "You lose! Press Enter to Restart!";
+        }
+        else
+        {
+            textBox.text = "Score: " + level.score;
+        }
+    }
+}
