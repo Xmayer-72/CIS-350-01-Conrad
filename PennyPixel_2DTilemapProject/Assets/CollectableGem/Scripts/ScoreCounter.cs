dc7bc33985bf1b3030b0b7405019aa8689d5bcbc
Xmayer-72 <conrad.mayer72@gmail.com>
2022-03-01 19:21:13 -0600

    Final 5A work

---
+++ PennyPixel_2DTilemapProject/Assets/CollectableGem/Scripts/ScoreCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreCounter : MonoBehaviour
+{
+    private Text textBox;
+
+    private bool scored = false;
+    public static int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox = GameObject.FindGameObjectWithTag("Score").GetComponent<Text>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player")&&!scored)
+        {
+            score++;
+            scored = true;
+        }
+    }
+
+    private void Update()
+    {
+        textBox.text = "Score: " + score;
+    }
+}
---
+++ PennyPixel_2DTilemapProject/Assets/LevelOver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelOver : MonoBehaviour
+{
+    private Text textBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textBox = GameObject.FindGameObjectWithTag("Win").GetComponent<Text>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            textBox.text = "You Win!";
+        }
+    }
+}
