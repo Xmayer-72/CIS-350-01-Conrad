020804c90af2088fe46c42a0f752a6d0d4847966
Xmayer-72 <conrad.mayer72@gmail.com>
2022-03-01 18:23:11 -0600

    Assignment 5 work

--- PennyPixel_2DTilemapProject/Assets/PennyPixel/Scripts/PlayerPlatformerController.cs
+++ PennyPixel_2DTilemapProject/Assets/PennyPixel/Scripts/PlayerPlatformerController.cs
@@ -48,7 +48,8 @@ public class PlayerPlatformerController : PhysicsObject {
         }
 
         animator.SetBool ("grounded", grounded);
-        animator.SetFloat ("velocityX", Mathf.Abs (velocity.x) / maxSpeed);
+        animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
+        animator.SetFloat("velocityY", velocity.y);
 
         targetVelocity = move * maxSpeed;
     }
