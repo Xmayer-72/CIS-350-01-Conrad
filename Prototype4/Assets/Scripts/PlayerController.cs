b776e213dd7824fb62af81a24190f6c2deee5265
Xmayer-72 <conrad.mayer72@gmail.com>
2022-04-06 14:19:45 -0500

    Prototype4 initial work

---
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementForce;
+
+    private Rigidbody rb;
+
+    private GameObject focal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        focal = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+
+        rb.AddForce(focal.transform.forward * movementForce * verticalInput, ForceMode.Force);
+    }
+}
