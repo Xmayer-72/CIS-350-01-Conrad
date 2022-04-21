e97a3b0ad3ee2a0fba0ed8b4a5cd1ca6a4bcf199
Xmayer-72 <conrad.mayer72@gmail.com>
2022-04-21 13:35:56 -0500

    Challenge 5

---
+++ Challenge 5/Assets/Challenge 5/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    private TextMeshProUGUI text;
+    [SerializeField] private GameManagerX game;
+    public float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 60;
+
+        text = GetComponent<TextMeshProUGUI>();
+
+        game = GameObject.Find("Game Manager").GetComponent<GameManagerX>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (game.isGameActive)
+        {
+            time -= Time.deltaTime;
+            text.text = "Time: " + Mathf.Round(time);
+        }
+
+        if (time <= 0)
+        {
+            game.GameOver();
+        }
+    }
+}
