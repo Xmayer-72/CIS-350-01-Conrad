74d60f57ea4867f477bf91674e5c2c903bad0555
Xmayer-72 <conrad.mayer72@gmail.com>
2022-04-26 14:19:28 -0500

    Assignment 9 initial work

---
+++ Assignment 9/Assets/Scripts/MoveTo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour
+{
+    public Transform target;
+
+    private NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        agent.destination = target.position;
+    }
+}
---
+++ Assignment 9/Assets/Scripts/MoveToMouseClick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToMouseClick : MonoBehaviour
+{
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+    private void Start()
+    {
+        cam = Camera.main;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+}
