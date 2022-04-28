7c657fc09176e4467ad08cfc345066b18f08f3c2
Xmayer-72 <conrad.mayer72@gmail.com>
2022-04-28 16:31:06 -0500

    Assignment 9 work

---
+++ Assignment 9/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+using UnityEngine.AI;
+using System;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+    public ThirdPersonCharacter character;
+
+    public GameObject player;
+
+    public float followDist;
+
+    private void Start()
+    {
+        cam = Camera.main;
+
+        character = GetComponent<ThirdPersonCharacter>();
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void Update()
+    {
+        moveEnemy();
+    }
+
+    private void moveEnemy()
+    {
+        float targetDist = Vector3.Distance(transform.position, player.transform.position);
+
+        if (targetDist > agent.stoppingDistance && targetDist < followDist)
+        {
+            agent.SetDestination(player.transform.position);
+            character.Move(agent.desiredVelocity, false, false);
+        }
+        else
+        {
+            agent.SetDestination(transform.position);
+            character.Move(Vector3.zero, false, false);
+        }
+    }
+}
